8292338f8dea06313e566055ae0f7e194822fd76
Egor I <egor.ivanov@pm.me>
2026-02-12 19:05:53 +0100

    adds allure3 config
    Signed-off-by: Egor I <egor.ivanov@pm.me>

---
+++ allurerc.mjs
@@ -0,0 +1,67 @@
+import { defaultChartsConfig, defineConfig } from "allure";
+
+const chartLayout = [
+  {
+    type: "trend",
+    dataType: "status",
+    mode: "percent",
+  },
+  {
+    type: "trend",
+    dataType: "status",
+    limit: 10,
+  },
+  {
+    title: "Custom Status Trend",
+    type: "trend",
+    dataType: "status",
+    mode: "percent",
+    limit: 15,
+  },
+  {
+    type: "trend",
+    dataType: "status",
+    limit: 15,
+    metadata: {
+      executionIdAccessor: (executionOrder) => `build-${executionOrder}`,
+      executionNameAccessor: (executionOrder) => `build #${executionOrder}`,
+    },
+  },
+  {
+    type: "trend",
+    dataType: "severity",
+    limit: 15,
+  },
+  {
+    type: "pie",
+  },
+  {
+    type: "pie",
+    title: "Custom Pie",
+  },
+];
+
+export default defineConfig({
+  name: "Allure Report",
+  output: "./allure-report",
+  historyPath: "./history.jsonl",
+  plugins: {
+    log: {
+      options: {
+        groupBy: ["feature"],
+        withTrace: false,
+        allSteps: false,
+      },
+    },
+    testops: {
+      options: {
+        launchName: `Jenkins agent sends results using allure3 (${new Date().toISOString()})`,
+      },
+    },
+  },
+  variables: {
+    env_variable: "unknown",
+    greeting: "¡Hola todos!",
+  },
+  environments: {},
+});
