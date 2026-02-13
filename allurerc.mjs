e711493c0f2c59ef34ed94651ac00c2cbe53eb14
Egor I <egor.ivanov@pm.me>
2026-02-13 09:24:16 +0100

    rm dasboard layout
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- allurerc.mjs
+++ allurerc.mjs
@@ -1,45 +1,5 @@
 import { defaultChartsConfig, defineConfig } from "allure";
 
-const chartLayout = [
-  {
-    type: "trend",
-    dataType: "status",
-    mode: "percent",
-  },
-  {
-    type: "trend",
-    dataType: "status",
-    limit: 10,
-  },
-  {
-    title: "Custom Status Trend",
-    type: "trend",
-    dataType: "status",
-    mode: "percent",
-    limit: 15,
-  },
-  {
-    type: "trend",
-    dataType: "status",
-    limit: 15,
-    metadata: {
-      executionIdAccessor: (executionOrder) => `build-${executionOrder}`,
-      executionNameAccessor: (executionOrder) => `build #${executionOrder}`,
-    },
-  },
-  {
-    type: "trend",
-    dataType: "severity",
-    limit: 15,
-  },
-  {
-    type: "pie",
-  },
-  {
-    type: "pie",
-    title: "Custom Pie",
-  },
-];
 
 export default defineConfig({
   name: "Allure Report",
@@ -52,9 +12,6 @@ export default defineConfig({
       },
     },
   },
-  variables: {
-    env_variable: "unknown",
-    greeting: "¡Hola todos!",
-  },
+  variables: { },
   environments: {},
 });
