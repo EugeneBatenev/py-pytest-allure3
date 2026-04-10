7c0064ba75531ad67f4c4e111e8d505c25884409
Egor I <egor.ivanov@pm.me>
2026-04-10 17:24:02 +0200

    adds single test to play with
    Signed-off-by: Egor I <egor.ivanov@pm.me>

---
+++ tests/single/test_single_test_n_steps.py
@@ -0,0 +1,18 @@
+import allure
+import pytest
+
+
+@allure.feature("test results processing")
+@allure.story("many unit tests")
+@allure.title("Assert a tuple 001 and it will fail")
+def test_unit_always_passing_001():
+    with allure.step("Start stuff"):
+        assert False, "Hahaha, do not leave Omsk"
+    with allure.step("Click stuff"):
+        assert True
+    with allure.step("Click stuff harder"):
+        assert True
+    with allure.step("Czech stuff"):
+        assert True
+    with allure.step("Check stuff"):
+        assert True
